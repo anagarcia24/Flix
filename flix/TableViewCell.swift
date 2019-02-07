b509aee7caab16923f47c58d13945e1741e88068
Garcia <garcia@Garcias-MacBook-Pro.local>
2019-02-07 13:28:32 -0600

    finished project

---
+++ flix/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  flix
+//
+//  Created by Garcia on 1/29/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
